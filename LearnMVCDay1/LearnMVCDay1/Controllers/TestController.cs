18eb9efa497f2e4d227b1b34293eb50c08d8c3d9
M R <mraduenzel@comcast.net>
2016-04-11 19:36:37 -0500

    First build

---
+++ LearnMVCDay1/LearnMVCDay1/Controllers/TestController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LearnMVCDay1.Models;
+
+namespace LearnMVCDay1.Controllers
+{
+    public class TestController : Controller
+    {
+        public string GetString()
+        {
+            return "Hello World is so yesterday. It&rsquo;s time for wassup bro :0";
+        }
+
+        public ActionResult GetView()
+        {
+            Employee emp = new Employee
+            {
+                FirstName = "Mark",
+                LastName = "Raduenzel",
+                Salary = 200000
+            };
+
+            ViewData["Employee"] = emp;
+
+            return View("MyView");
+        }
+    }
+}
---
+++ LearnMVCDay1/LearnMVCDay1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LearnMVCDay1.MvcApplication" Language="C#" %>
